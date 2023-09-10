c19480b5d1b6630c766acec7ec578b2ba3d6a08a
sawyerpatr1 <sawyerpatr1@ecs.vuw.ac.nz>
2023-09-11 01:09:38 +1200

    fixed indentation

--- src/client/java/com/example/ThreatTracker.java
+++ src/client/java/com/example/ThreatTracker.java
@@ -18,28 +18,28 @@ public class ThreatTracker implements EndTick {
     private Boolean stopped = true;
     
     @Override
-public void onEndTick(MinecraftClient client) {
-    if (client != null && client.player != null) {
-        // Check if there are any mobs categorized as hostile within the blockRadius
-        threatLevel = client.world.getEntitiesByClass(net.minecraft.entity.mob.HostileEntity.class, client.player.getBoundingBox().expand(blockRadius, blockRadius, blockRadius), EntityPredicates.EXCEPT_SPECTATOR).size();
+    public void onEndTick(MinecraftClient client) {
+        if (client != null && client.player != null) {
+            // Check if there are any mobs categorized as hostile within the blockRadius
+            threatLevel = client.world.getEntitiesByClass(net.minecraft.entity.mob.HostileEntity.class, client.player.getBoundingBox().expand(blockRadius, blockRadius, blockRadius), EntityPredicates.EXCEPT_SPECTATOR).size();
 
-        if (threatLevel > 0 && stopped) {
-            ModSounds.changeRegion(client);
-            ModSounds.currentRegion.play(client);
-            client.player.sendMessage(Text.of("Playing music"), false);
-            lastPlayed = 0;
-            stopped = false;
-        } else if (threatLevel == 0 && !stopped) {
-            if (lastPlayed >= maxTime) {
-                ModSounds.currentRegion.stop(client);
-                ModSounds.currentRegion.randomizeLayers();
-                client.player.sendMessage(Text.of("Stopping music"), false);
-                stopped = true;
+            if (threatLevel > 0 && stopped) {
+                ModSounds.changeRegion(client);
+                ModSounds.currentRegion.play(client);
+                client.player.sendMessage(Text.of("Playing music"), false);
                 lastPlayed = 0;
-            } else {
-                lastPlayed++;
+                stopped = false;
+            } else if (threatLevel == 0 && !stopped) {
+                if (lastPlayed >= maxTime) {
+                    ModSounds.currentRegion.stop(client);
+                    ModSounds.currentRegion.randomizeLayers();
+                    client.player.sendMessage(Text.of("Stopping music"), false);
+                    stopped = true;
+                    lastPlayed = 0;
+                } else {
+                    lastPlayed++;
+                }
             }
         }
     }
-}
 }
